466a5f7935a7ac3b54ba9c22896cfa66962bdc0e
cvzi <cuzi@openmail.cc>
2023-03-27 10:23:10 +0200

    Add script to path

--- requirements/__main__.py
+++ requirements/__main__.py
@@ -8,11 +8,15 @@ import os
 import sys
 from .requirements import verbose
 
+
+def main():
+    verbose(sys.argv[1:])
+
+
 # If we are running from a wheel, add the wheel to sys.path
 if __package__ == '':
     path = os.path.dirname(os.path.dirname(__file__))
     sys.path.insert(0, path)
 
-
 if __name__ == "__main__":
-    verbose(sys.argv[1:])
+    main()
